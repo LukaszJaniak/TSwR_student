ca583aa6f4fe2b66cd28db3e8ea139d7f9ed27b3
lukasz <lukjani96@gmail.com>
2023-06-09 14:33:43 +0200

    commit

--- controllers/adrc_joint_controller.py
+++ controllers/adrc_joint_controller.py
@@ -8,17 +8,23 @@ class ADRCJointController(Controller):
         self.b = b
         self.kp = kp
         self.kd = kd
+        self.last_u = 0
 
-        A = None
-        B = None
-        L = None
-        W = None
+        A = np.eye(3,k=1)
+        B = np.array([[0], [self.b], [0]])
+        L = np.array([[3*p], [3*p**2], [p**3]])
+        W = np.array([[1, 0, 0]])
         self.eso = ESO(A, B, W, L, q0, Tp)
+        
 
     def set_b(self, b):
-        ### TODO update self.b and B in ESO
-        return NotImplementedError
+        self.eso.set_B(np.array([[0], [b], [0]]))
 
     def calculate_control(self, x, q_d, q_d_dot, q_d_ddot):
-        ### TODO implement ADRC
-        return NotImplementedError
+        q, q_dot = x
+        q_est, q_est_dot, f = self.eso.get_state()
+        self.eso.update(q, self.last_u)
+        v = self.kp * (q_d - q) + self.kd * (q_d_dot - q_est_dot) + q_d_ddot
+        u = (v - f) / self.b
+        self.last_u = u
+        return u
--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -12,4 +12,11 @@ class FeedbackLinearizationController(Controller):
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+        q1, q2, q1_dot, q2_dot = x
+        q_t = x[:2]
+        q_t_dot = x[2:]
+        K_d = [[25, 0], [0, 25]]
+        K_p = [[60, 0], [0, 60]]
+        v = q_r_ddot + K_d @ (q_r_dot - q_t_dot) + K_p @ (q_r - q_t)
+        tau = self.model.M(x) @ v[:, np.newaxis] + self.model.C(x) @ q_r_dot[:, np.newaxis]
+        return tau
